9f1ac42683f0a09cebdf91f1fef35d214e20a262
itimdad <iamfirdausi01@gmail.com>
2025-12-24 19:48:56 +0530

    added more classes in structrure

--- src/main/java/com/imdad/service/UserService.java
+++ src/main/java/com/imdad/service/UserService.java
@@ -1,5 +1,19 @@
 package com.imdad.service;
 
+import com.imdad.binding.DashboardResponse;
+import com.imdad.binding.LoginForm;
+import com.imdad.binding.SignUpForm;
+import com.imdad.binding.UnlockForm;
+
 public interface UserService {
 
+	public boolean registerUser(SignUpForm signUpForm);
+	
+	public boolean loginUser(LoginForm loginForm);
+	
+	public boolean unlockAccount(UnlockForm unlockForm);
+	
+	public boolean forgotPassword(String email);
+	
+	public DashboardResponse dashboardData();
 }
--- src/main/java/com/imdad/service/UserServiceImpl.java
+++ src/main/java/com/imdad/service/UserServiceImpl.java
@@ -1,5 +1,40 @@
 package com.imdad.service;
 
+import com.imdad.binding.DashboardResponse;
+import com.imdad.binding.LoginForm;
+import com.imdad.binding.SignUpForm;
+import com.imdad.binding.UnlockForm;
+
 public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean registerUser(SignUpForm signUpForm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean loginUser(LoginForm loginForm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unlockAccount(UnlockForm unlockForm) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean forgotPassword(String email) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public DashboardResponse dashboardData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
